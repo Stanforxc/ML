0d780a24d4e9d37f6a22d32ba37a46c519d3a114
stanforxc <13661785732@163.com>
2016-08-03 11:16:18 +0800

    随机梯度

---
+++ Logistic/logRegres.py
@@ -0,0 +1,76 @@
+#coding:utf-8
+from numpy import *
+
+#Logistic回归梯度上升优化算法
+def loadDataSet():
+    dataMat = [];labelMat = []
+    fr = open('testSet.txt')
+    for line in fr.readlines():
+        lineArr = line.strip().split()
+        dataMat.append([1.0,float(lineArr[0]),float(lineArr[1])])
+        labelMat.append(int(lineArr[2]))
+    return dataMat,labelMat
+
+#sigmoid函数
+def sigmoid(inX):
+    return 1.0/(1+exp(-inX))
+
+#梯度上升函数
+def gradAscent(dataMatIn,classLabels):
+    dataMatrix = mat(dataMatIn)
+    labelMat = mat(classLabels).transpose()
+    m,n = shape(dataMatrix)
+    alpha = 0.001 #学习率
+    maxCycles = 500
+    weights = ones((n,1))
+    for k in range(maxCycles):
+        h = sigmoid(dataMatrix*weights)
+        error = (labelMat - h)
+        weights = weights + alpha*dataMatrix.transpose()*error
+    return weights
+
+#画出数据集和logistic回归最佳拟合直线
+def plotBestFit(weights):
+    import matplotlib.pyplot as plt
+    dataMat,labelMat = loadDataSet()
+    dataArr = array(dataMat)
+    n = shape(dataArr)[0]
+    xcord1 = []; ycord1 = []
+    xcord2 = []; ycord2 = []
+    for i in range(n):
+        if int(labelMat[i]) == 1:
+            xcord1.append(dataArr[i,1]);ycord1.append(dataArr[i,2])
+        else:
+            xcord2.append(dataArr[i,1]);ycord2.append(dataArr[i,2])
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    ax.scatter(xcord1,ycord1,s=30,c='red',marker='s')
+    ax.scatter(xcord2,ycord2,s=30,c='green')
+    x = arange(-3.0,3.0,0.1)
+    y = (-weights[0]-weights[1]*x)/weights[2]
+    ax.plot(x,y)
+    plt.xlabel('X1');plt.ylabel('X2')
+    plt.show()
+"""
+dataArr,labelMat = loadDataSet()
+weights =  gradAscent(dataArr,labelMat)
+plotBestFit(weights.getA())
+"""
+#随机梯度上升算法
+def stocGradAscent0(dataMatrix,classLabels,numIter=150):
+    m,n = shape(dataMatrix)
+    weights = ones(n)
+    for j in range(numIter):
+        dataIndex = range(m)
+        for i in range(m):
+            alpha = 4/(1.0+j+i)+0.01
+            randIndex = int(random.uniform(0,len(dataIndex)))
+            h = sigmoid(sum(dataMatrix[randIndex]*weights))
+            error = classLabels[randIndex] - h
+            weights = weights + alpha*error*dataMatrix[randIndex]
+            del(dataIndex[randIndex])
+    return weights
+
+dataArr,labelMat = loadDataSet()
+weights = stocGradAscent0(array(dataArr),labelMat)
+plotBestFit(weights)
